3951fca6a0281613ad33d3653605d6bbca57580e
stucki <stucki@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-09-08 18:38:22 +0000

    small ChangeLog update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cc_langguess/trunk@3687 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "cc_langguess"
+#
+# Auto generated 30-08-2006 10:11
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'textLang: Lang guess',
+	'description' => 'Language guessing service using an external Perl script.',
+	'category' => 'services',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Rene Fritz',
+	'author_email' => 'r.fritz@colorcube.de',
+	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '3.0.0-',
+			'typo3' => '3.5.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:62:{s:12:"ext_icon.gif";s:4:"62e5";s:14:"ext_tables.php";s:4:"83cd";s:13:"lang_guess.pl";s:4:"aa2a";s:14:"doc/manual.sxw";s:4:"09bb";s:19:"lang_guess/Guess.pm";s:4:"0ddf";s:23:"lang_guess/Guess.pm.org";s:4:"d0aa";s:32:"lang_guess/train/ar-arabic.train";s:4:"3091";s:28:"lang_guess/train/azeri.train";s:4:"36c0";s:35:"lang_guess/train/bg-bulgarian.train";s:4:"5aab";s:33:"lang_guess/train/bn-bengali.train";s:4:"9502";s:30:"lang_guess/train/cebuano.train";s:4:"b451";s:31:"lang_guess/train/cs-czech.train";s:4:"f7c5";s:31:"lang_guess/train/cy-welsh.train";s:4:"ec46";s:32:"lang_guess/train/da-danish.train";s:4:"1c17";s:32:"lang_guess/train/de-german.train";s:4:"6e0c";s:33:"lang_guess/train/en-english.train";s:4:"ff00";s:33:"lang_guess/train/es-spanish.train";s:4:"d248";s:34:"lang_guess/train/et-estonian.train";s:4:"20dc";s:31:"lang_guess/train/fa-farsi.train";s:4:"a203";s:33:"lang_guess/train/fi-finnish.train";s:4:"9602";s:32:"lang_guess/train/fr-french.train";s:4:"5d21";s:31:"lang_guess/train/ha-hausa.train";s:4:"7bcc";s:31:"lang_guess/train/hawaiian.train";s:4:"5a3d";s:31:"lang_guess/train/hi-hindi.train";s:4:"a31c";s:34:"lang_guess/train/hr-croatian.train";s:4:"2850";s:35:"lang_guess/train/hu-hungarian.train";s:4:"2856";s:36:"lang_guess/train/id-indonesian.train";s:4:"8836";s:35:"lang_guess/train/is-icelandic.train";s:4:"75d9";s:33:"lang_guess/train/it-italian.train";s:4:"8b49";s:32:"lang_guess/train/kk-kazakh.train";s:4:"7b06";s:30:"lang_guess/train/klingon.train";s:4:"6be7";s:32:"lang_guess/train/ky-kyrgyz.train";s:4:"1bd1";s:31:"lang_guess/train/la-latin.train";s:4:"1a9f";s:36:"lang_guess/train/lt-lithuanian.train";s:4:"b933";s:33:"lang_guess/train/lv-latvian.train";s:4:"99dd";s:36:"lang_guess/train/mk-macedonian.train";s:4:"bf2c";s:35:"lang_guess/train/mn-mongolian.train";s:4:"e2a9";s:32:"lang_guess/train/ne-nepali.train";s:4:"0d3f";s:31:"lang_guess/train/nl-dutch.train";s:4:"76bc";s:35:"lang_guess/train/no-norwegian.train";s:4:"40d5";s:29:"lang_guess/train/pidgin.train";s:4:"aecf";s:32:"lang_guess/train/pig_latin.train";s:4:"6254";s:32:"lang_guess/train/pl-polish.train";s:4:"4d77";s:32:"lang_guess/train/ps-pashto.train";s:4:"1ad2";s:36:"lang_guess/train/pt-portuguese.train";s:4:"6a9d";s:34:"lang_guess/train/ro-romanian.train";s:4:"b238";s:33:"lang_guess/train/ru-russian.train";s:4:"c3e4";s:32:"lang_guess/train/sk-slovak.train";s:4:"c541";s:33:"lang_guess/train/sl-slovene.train";s:4:"64bc";s:32:"lang_guess/train/so-somali.train";s:4:"207b";s:34:"lang_guess/train/sq-albanian.train";s:4:"0467";s:33:"lang_guess/train/sr-serbian.train";s:4:"69cb";s:33:"lang_guess/train/sv-swedish.train";s:4:"8cfb";s:33:"lang_guess/train/sw-swahili.train";s:4:"ee73";s:33:"lang_guess/train/tl-tagalog.train";s:4:"e871";s:33:"lang_guess/train/tr-turkish.train";s:4:"94f6";s:35:"lang_guess/train/uk-ukrainian.train";s:4:"53de";s:31:"lang_guess/train/ukranian.train";s:4:"53de";s:30:"lang_guess/train/ur-urdu.train";s:4:"f778";s:31:"lang_guess/train/uz-uzbek.train";s:4:"41ca";s:36:"lang_guess/train/vi-vietnamese.train";s:4:"a431";s:32:"sv1/class.tx_cclangguess_sv1.php";s:4:"0236";}',
+);
+
+?>
